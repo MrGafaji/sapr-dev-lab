bea3659f38fd668a7e2bd80420d00d104d8af138
Maikel <maikel.gafaji@gmail.com>
2020-02-07 19:24:18 +0100

    registering circles in object and move each circle separate

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,24 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+  import Nav from "../components/Nav.svelte";
 
-	export let segment;
+  export let segment;
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 100%;
-		height: 100%;
-		background-color: black;
-		margin: auto auto;
-		box-sizing: border-box;
-	}
+  main {
+    position: relative;
+    max-width: 100%;
+    height: 100%;
+    background-color: black;
+    color: white;
+    margin: auto auto;
+    box-sizing: border-box;
+    font-size: 2em;
+  }
 </style>
 
 <!-- <Nav {segment}/> -->
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,35 +1,74 @@
 <script>
+  import { onMount } from "svelte";
   const viewBoxWidth = 200;
   const viewBoxHeight = 200;
+  const numberOfCircles = 10;
+
+  let registeredCircles = [];
+  let start;
+  let progress;
+  let pause = false;
 
   function step(timestamp) {
     if (!start) start = timestamp;
-    var progress = timestamp - start;
-    moveCircle("circle");
-    if (progress < 20000) {
+    progress = timestamp - start;
+    for (let i = 0; i < registeredCircles.length; i++) {
+      moveCircle(i, progress);
+    }
+    if (progress < 100000 && !pause) {
       window.requestAnimationFrame(step);
     }
   }
 
-  window.requestAnimationFrame(step);
+  function populateRegisteredCircles() {
+    for (let i = 0; i < numberOfCircles; i++) {
+      var circle = {};
+      circle.cx = Math.random() * viewBoxWidth;
+      circle.cy = Math.random() * viewBoxHeight;
+      circle.r = 5;
+      circle.id = "circle-" + i;
 
-  function generateCircle(size) {
-    var circle = document.createElement("circle");
-    circle.setAttribute("cx", Math.random() * viewBoxWidth);
-    circle.setAttribute("cy", Math.random() * viewBoxHeight);
-    circle.setAttribute("r", size);
-    circle.style.fill = "white";
-    document.querySelector("#viewbox").appendChild(circle);
+      circle.initial = circle;
+
+      registeredCircles.push(circle);
+    }
   }
 
-  function moveCircle(circleId) {
-    var circle = document.getElementById(circleId);
-    circle.setAttribute("cx", Math.random() * viewBoxWidth);
-    circle.setAttribute("cy", Math.random() * viewBoxHeight);
+  function moveCircle(circleId, prog) {
+    var circle = document.getElementById("circle-" + circleId);
+    if (circle) {
+      circle.setAttribute(
+        "cx",
+        registeredCircles[circleId].initial.cx +
+          Math.sin(prog / 1000) * registeredCircles[circleId].initial.cx
+      );
+      circle.setAttribute(
+        "cy",
+        registeredCircles[circleId].initial.cy +
+          Math.sin(prog / 2000) * registeredCircles[circleId].initial.cy
+      );
+      circle.setAttribute(
+        "r",
+        registeredCircles[circleId].initial.r +
+          Math.sin(prog / 1500) * registeredCircles[circleId].initial.r
+      );
+    } else {
+      console.log(" No circles found");
+    }
   }
 
-  window.generateCircle = generateCircle;
-  window.moveCircle = moveCircle;
+  function onPause(p) {
+    pause = !p;
+    p && window.requestAnimationFrame(step);
+  }
+
+  populateRegisteredCircles();
+  onMount(() => {
+    window.registeredCircles = registeredCircles;
+    window.populateRegisteredCircles = populateRegisteredCircles;
+    window.moveCircle = moveCircle;
+    window.requestAnimationFrame(step);
+  });
 </script>
 
 <style>
@@ -47,5 +86,12 @@
   viewBox="0 0 {viewBoxWidth}
   {viewBoxHeight}"
   xmlns="http://www.w3.org/2000/svg">
-  <circle id="circle" cx="100" cy="100" r="5" />
+  {#each registeredCircles as { id, cx, cy, r }}
+    <circle {id} {cx} {cy} {r} />
+  {:else}
+    <h1>No circles today!</h1>
+  {/each}
+
 </svg>
+
+<button on:click={onPause(pause)}>pause</button>
